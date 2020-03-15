7d45a7e060e13425601154c713858b10beb2dbab
collect_vood <yannischu123@gmail.com>
2020-03-15 23:59:36 +0100

    Instant Buy 1.0.2 - Use perm added

--- InstantBuy.cs
+++ InstantBuy.cs
@@ -1,18 +1,35 @@
 ﻿
 namespace Oxide.Plugins
 {
-    [Info("Instant Buy", "Jake_Rich/collect_vood", "1.0.1")]
+    [Info("Instant Buy", "Jake_Rich/collect_vood", "1.0.2")]
     [Description("Vending Machine has no delay")]
 
     public class InstantBuy : CovalencePlugin
     {
+        #region Constants
+
+        private const string permUse = "instantbuy.use";
+
+        #endregion
+
+        #region Hooks
+
+        private void Init()
+        {
+            permission.RegisterPermission(permUse, this);
+        }
+
         private object OnBuyVendingItem(VendingMachine machine, BasePlayer player, int sellOrderID, int amount)
         {
             if (machine == null || player == null) return null;
+
+            if (!permission.UserHasPermission(player.UserIDString, permUse)) return null;
 
             machine.ClientRPC<int>(null, "CLIENT_StartVendingSounds", sellOrderID);
             machine.DoTransaction(player, sellOrderID, amount);
             return false;
         }
+
+        #endregion
     }
 }
