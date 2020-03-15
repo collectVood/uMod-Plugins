00f1cac71217054953a0eb68a7785b5adb7e21a4
collect_vood <yannischu123@gmail.com>
2020-03-15 15:35:55 +0100

    InstantBuy 1.0.1

---
+++ InstantBuy.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Oxide.Plugins
+{
+    [Info("Instant Buy", "Jake_Rich/collect_vood", "1.0.1")]
+    [Description("Vending Machine has no delay")]
+
+    public class InstantBuy : CovalencePlugin
+    {
+        private object OnBuyVendingItem(VendingMachine machine, BasePlayer player, int sellOrderID, int amount)
+        {
+            if (machine == null || player == null) return null;
+
+            machine.ClientRPC<int>(null, "CLIENT_StartVendingSounds", sellOrderID);
+            machine.DoTransaction(player, sellOrderID, amount);
+            return false;
+        }
+    }
+}
